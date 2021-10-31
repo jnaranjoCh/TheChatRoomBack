2d541515afc70db313e757b0df7f499a3c4f78e8
jnaranjoCh <jnaranjo.facyt.uc@gmail.com>
2021-10-31 13:46:02 -0600

    ajuste en cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    "allowedHeaders": "Content-type",
+    "credentials": true
   });
   await app.listen( process.env.PORT || 3000 );
 }
--- src/socket-io/chatRoom.gateway.ts
+++ src/socket-io/chatRoom.gateway.ts
@@ -1,11 +1,11 @@
 import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({ namespace: "theChatRoomFront", allowEIO3: true, cors: {
+@WebSocketGateway({ allowEIO3: true, cors: {
     origin: ["*", "http://localhost:8080"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    transports: ['websocket', 'polling'],
     credentials: true,
-    transports: ['websocket', 'polling']
 }})
 export class ChatGateway {
 
